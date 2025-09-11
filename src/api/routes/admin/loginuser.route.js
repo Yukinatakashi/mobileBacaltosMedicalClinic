6233565710f6766b32657d08a32ee9e7603b011c
Yuuko <yuukomochi30@gmail.com>
2025-09-11 19:31:41 +0800

    .

--- src/api/routes/admin/loginuser.route.js
+++ src/api/routes/admin/loginuser.route.js
@@ -1,6 +1,6 @@
 // src/api/routes/login.route.js
 const express = require('express');
-const { loginUser } = require('../handlers/loginuser.handler'); // Fixed path
+const { loginUser } = require('../../handler/loginuser.js');
 
 const router = express.Router();
 
